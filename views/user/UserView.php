dff99ee61e943f2dee53e52f59669114dc076bc5
Meilo <ludovic.elice@gmail.com>
2015-09-13 22:31:29 +0200

    intégration de la page user

--- views/user/UserView.php
+++ views/user/UserView.php
@@ -1,5 +1,15 @@
 <div class="row bloc-user">
 	<div class="col-md-12 container-user">
-		
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-md-5 content-user">
+				<h1>Se connecter</h1>
+				<input type="text" class="form-control" placeholder="Identifiant" /><br>
+				<input type="text" class="form-control" placeholder="Mot de passe" /><br>
+				<button class="btn btn-lg btn-success">Connexion</button><br>
+				<p><a href="#">Je n'ai pas de compte</a></p>
+				<p><a href="#">J'ai oublier mon mot de passe</a></p>
+			</div>
+		</div>
 	</div>
 </div>
